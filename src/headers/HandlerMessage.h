0dfcac8478d134d7dfd0a3db749544f9ad1c6796
Maksim Safronov <safranov01@list.ru>
2025-10-21 09:58:23 +0000

    remove unnecessary file

--- src/headers/HandlerMessage.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <memory>
-#include "Connection.h" 
-#include "Message.h"
-
-class HandlerMessage {
-    public:
-        virtual void handler(std::unique_ptr<BaseMessage>&& msg, const std::unique_ptr<Connection>& connection = nullptr) = 0;
-};
